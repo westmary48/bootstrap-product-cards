31222d5724dcbf070142d24a554c538c79c867fd
Mary West <westmarye1219@gmail.com>
2019-03-20 18:20:20 -0500

    wip

--- main.js
+++ main.js
@@ -66,9 +66,26 @@ const printToDom = (divId, textToPrint) => {
 const productCards = () => {
     let domString = '';
     products.forEach(product => {
-        domString += `<div class ='container col-lg-4 row align-items-center'>`
-        domString += `<h3 class="row-sm">${product.title}</h3>`
+        domString += `<div class = ' col-sm-4 product'>`
+        domString += `<div class = "card">`
+        domString += `<div class = 'card-header'>${product.title}</div>`
         domString += `</div>`
+        domString += `<img class="card-img-top" src="${product.imageUrl}" alt="Card image cap">`
+        domString += `<div class="card-body">`
+        domString += `<p class="card-text">${product.description}</p>`
+        domString += `<p class="card-text"></p>`
+        domString += ` </div>`
+        domString += `</div>`
+        domString += `</div>`
+
+        // domString += `<h3>${product.description}</h3>`
+        // domString += `<h3>${product.imageUrl}</h3>`
+        // domString += `<h3>${product.size}</h3>`
+        // domString += `<h3>${product.weight}</h3>`
+        // domString += `<h3>${product.price1}</h3>`
+        // domString += `<h3>${product.price2}</h3>`
+        // domString += `<h3>${product.price3}</h3>`
+
 
     });
     printToDom('product-cards', domString)
